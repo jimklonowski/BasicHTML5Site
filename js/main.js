f01015f0a34192ec42e36be81337223a5c7384ee
Jim Klonowski <jim@jimklonowski.com>
2015-09-28 18:56:39 -0500

    added silly mood form to brush up on javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+function showMood(){
+	name = document.getElementById('name').value;
+	mood = document.getElementById('mood').value;
 
+	//validation
+	if(!name || !mood){
+		alert("Please fill in all fields.");
+		return false;
+	}
+
+	moodString = name + " is " + mood + " today! ";
+	
+	//ifelse fun
+	if(mood == "happy"){
+		smiley = ":)";
+	}else if(mood == "sad"){
+		smiley = ":(";
+	}else{
+		smiley = ":|";
+	}
+
+	holder = document.getElementById('holder');
+	holder.innerHTML = moodString+smiley;
+}
+
+function clearMood(){
+	document.getElementById('moodForm').reset();
+	holder = document.getElementById('holder');
+	holder.innerHTML = "";
+}
